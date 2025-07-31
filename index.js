bde8d84fef9a02915cecbb8360b324f7c504bfc1
Emily Harbeck <eharbeck@me.com>
2025-07-31 17:06:56 +1000

    passing 6 of 9 tests

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ saturdayFun()
 function saturdayFun(activity = "roller-skate") {
     return(`This Saturday, I want to ${activity}!`);
 }
-function mondayWork(activity = "go to the office") {
-    return(`This Monday, I will ${activity}.`);
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
 }
 mondayWork()
